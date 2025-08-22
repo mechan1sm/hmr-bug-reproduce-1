41af92a983d6aaf7c5e44737822b062bbdbd2c1f
mechan1sm <78693192+mechan1sm@users.noreply.github.com>
2025-08-22 20:35:36 +0300

    add reactivity to original src for rsbuild example

--- rsbuild-actual/src-original/App.tsx
+++ rsbuild-actual/src-original/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { createSignal } from 'solid-js';
 
 const App = () => {
+  const [count, setCount] = createSignal(1);
+  const increment = () => setCount(count() + 1);
   return (
     <div class="content">
       <h1>Rsbuild with Solid</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <button type="button" onClick={increment}>
+        {count()}
+      </button>
     </div>
   );
 };
